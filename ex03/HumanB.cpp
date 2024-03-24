69cda88b16c4b41c323e5c9d892b7ec69385d9e0
Kate Sansom <katesansom@Kates-MacBook-Air.local>
2024-03-24 09:16:59 +0100

    ex03

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -21,8 +21,8 @@ void	HumanB::setWeapon(Weapon& weapon) {
 	this->_weapon = &weapon;
 }
 void	HumanB::attack(void) const {
-	if (this->_weapon != NULL && this->_weapon.getType() != "")
-		std::cout << this->_name << " attacks with their " << this->_weapon.getType() << std::endl;
+	if (this->_weapon != NULL && this->_weapon->getType() != "")
+		std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
 	else
 		std::cout << this->_name << " doesn't have a weapon" << std::endl;
 }
